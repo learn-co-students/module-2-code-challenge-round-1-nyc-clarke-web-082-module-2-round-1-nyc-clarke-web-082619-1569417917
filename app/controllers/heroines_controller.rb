6480921a7d44908e7270e506cf93e63102e0cc82
github username <github email address>
2019-09-25 11:01:35 -0400

    Ken

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,36 @@
 class HeroinesController < ApplicationController
+  
+  before_action :find_heroine, only: [:show]
+  
+  def home
+    render :home
+  end
+  
   def index
     @heroines = Heroine.all
   end
+
+  def show
+  
+  end
+
+  def new
+    @heroine = Heroine.new
+    @powers = Power.all
+  end
+
+  def create
+    @heroine = Heroine.create(heroine_params)
+  end
+
+  private
+
+  def find_heroine
+    @heroine = Heroine.find(params[:id])
+  end
+
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name, :power_id)
+  end 
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  resources :powers, only: [:index]
-  resources :heroines, only: [:index]  
+  resources :powers, only: [:index, :new, :create, :show, :udpate, :edit, :delete]
+  resources :heroines, only: [:index, :new, :create, :show, :udpate, :edit, :delete]   
+
+  root 'heroines#home'
 end
