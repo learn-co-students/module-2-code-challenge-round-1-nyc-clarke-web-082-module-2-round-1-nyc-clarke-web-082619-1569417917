dfe5d97da6ae231d597e9670740d9b7b8d69069c
github username <github email address>
2019-09-25 10:11:05 -0400

    without bonus

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,31 @@
 class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
+    # if params.key?(:q)
+    #   @H = @heroines.select{|heroine| heroine.power.name == params[:q]}
+    #   @heroines = @H
+    # end
+  end
+
+  def new
+    @heroine = Heroine.new
+  end
+
+  def show
+    @heroine = Heroine.find(params[:id])
+  end
+
+  def create
+    @heroine = Heroine.new(heroine_params)
+    if @heroine.save
+      redirect_to @heroine
+    else
+      flash[:error] = "Error: Heroines must have unique Super Names!"
+      render :new
+    end
+  end
+
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name, :power_id)
   end
 end
